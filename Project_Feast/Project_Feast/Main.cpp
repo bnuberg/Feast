f843c590ec7736cfd2d3de0ab7bab96efc9f5fc6
DESKTOP-N7L0VPF\Frenk-PC <frenk-timmer@outlook.com>
2017-04-19 12:42:17 +0200

    WIP Tried to implement minimalogre ui in our project

--- Project_Feast/Project_Feast/Main.cpp
+++ Project_Feast/Project_Feast/Main.cpp
@@ -16,6 +16,12 @@ Main::Main()
 :mRoot(0),
 mWindow(0),
 mCameraMan(0),
+mOverlaySystem(0),
+mDetailsPanel(0),
+mTrayMgr(0),
+mMouse(0),
+mKeyboard(0),
+mInputManager(0),
 mResourcesCfg(Ogre::StringUtil::BLANK),
 mPluginsCfg(Ogre::StringUtil::BLANK)
 {
@@ -24,7 +30,12 @@ mPluginsCfg(Ogre::StringUtil::BLANK)
 
 Main::~Main()
 {
-	
+	if (mTrayMgr) delete mTrayMgr;
+	if (mOverlaySystem) delete mOverlaySystem;
+
+	//Remove ourself as a Window listener
+	Ogre::WindowEventUtilities::removeWindowEventListener(mWindow, this);
+	windowClosed(mWindow);
 	delete mRoot;
 }
 
@@ -75,6 +86,10 @@ bool Main::go()
 	
 
 	mgr.mSceneMgr = mRoot->createSceneManager(Ogre::ST_GENERIC);
+
+	// initialize the OverlaySystem (changed for 1.9)
+	mOverlaySystem = new Ogre::OverlaySystem();
+	mgr.mSceneMgr->addRenderQueueListener(mOverlaySystem);
 	
 	mMainCamera->CameraInstance();
 
@@ -127,7 +142,43 @@ bool Main::go()
 	
 	// Initialize the input manager
 	mgr.mInputManager.InitInput(mWindow);
-	
+
+	//-------------------------------------------------------------------------------------
+	//create FrameListener
+	Ogre::LogManager::getSingletonPtr()->logMessage("*** Initializing OIS ***");
+	OIS::ParamList pl;
+	size_t windowHnd = 0;
+	std::ostringstream windowHndStr;
+
+	mWindow->getCustomAttribute("WINDOW", &windowHnd);
+	windowHndStr << windowHnd;
+	pl.insert(std::make_pair(std::string("WINDOW"), windowHndStr.str()));
+
+	mInputManager = OIS::InputManager::createInputSystem(pl);
+
+	mKeyboard = static_cast<OIS::Keyboard*>(mInputManager->createInputObject(OIS::OISKeyboard, true));
+	mMouse = static_cast<OIS::Mouse*>(mInputManager->createInputObject(OIS::OISMouse, true));
+
+	mMouse->setEventCallback(this);
+	mKeyboard->setEventCallback(this);
+
+	//Set initial mouse clipping size
+	windowResized(mWindow);
+
+	//Register as a Window listener
+	Ogre::WindowEventUtilities::addWindowEventListener(mWindow, this);
+
+	mInputContext.mKeyboard = mKeyboard;
+	mInputContext.mMouse = mMouse;
+	mTrayMgr = new OgreBites::SdkTrayManager("InterfaceName", mWindow, mInputContext, this);
+	mTrayMgr->hideCursor();
+
+	// create a params panel for displaying sample details
+	Ogre::StringVector items;
+	items.push_back("Test");
+
+	mDetailsPanel = mTrayMgr->createParamsPanel(OgreBites::TL_TOPRIGHT, "DetailsPanel", 200, items);
+
 	mRoot->addFrameListener(this);
 	mRoot->startRendering();
 	
@@ -153,6 +204,21 @@ bool Main::frameRenderingQueued(const Ogre::FrameEvent& evt)
 	if (!processUnbufferedInput(evt))
 		return false;
 
+	mTrayMgr->frameRenderingQueued(evt);
+
+	//Need to capture/update each device
+	mKeyboard->capture();
+	mMouse->capture();
+
+	if (!mTrayMgr->isDialogVisible())
+	{
+		mCameraMan->frameRenderingQueued(evt);   // if dialog isn't up, then update the camera
+		if (mDetailsPanel->isVisible())   // if details panel is visible, then update its contents
+		{
+			mDetailsPanel->setParamValue(0, "Test");
+		}
+	}
+
 	//Ogre::Real dist = (mgr.mCamera->getPosition() - mCameraMan->getTarget()->_getDerivedPosition()).length();
 	//mCameraMan->setYawPitchDist(mgr.mCamera->getOrientation().getYaw(), Ogre::Radian(1.0472), dist);
 	mCameraMan->setYawPitchDist(Ogre::Radian(0), Ogre::Radian(0.349066), Ogre::Real(380));
@@ -168,6 +234,68 @@ bool Main::processUnbufferedInput(const Ogre::FrameEvent& evt)
 	return true;
 }
 
+bool Main::keyPressed(const OIS::KeyEvent &arg)
+{
+	if (mTrayMgr->isDialogVisible()) return true;   // don't process any more keys if dialog is up
+	mCameraMan->injectKeyDown(arg);
+	return true;
+}
+
+bool Main::keyReleased(const OIS::KeyEvent &arg)
+{
+	mCameraMan->injectKeyUp(arg);
+	return true;
+}
+
+bool Main::mouseMoved(const OIS::MouseEvent &arg)
+{
+	if (mTrayMgr->injectMouseMove(arg)) return true;
+	mCameraMan->injectMouseMove(arg);
+	return true;
+}
+
+bool Main::mousePressed(const OIS::MouseEvent &arg, OIS::MouseButtonID id)
+{
+	if (mTrayMgr->injectMouseDown(arg, id)) return true;
+	mCameraMan->injectMouseDown(arg, id);
+	return true;
+}
+
+bool Main::mouseReleased(const OIS::MouseEvent &arg, OIS::MouseButtonID id)
+{
+	if (mTrayMgr->injectMouseUp(arg, id)) return true;
+	mCameraMan->injectMouseUp(arg, id);
+	return true;
+}
+
+//Adjust mouse clipping area
+void Main::windowResized(Ogre::RenderWindow* rw)
+{
+	unsigned int width, height, depth;
+	int left, top;
+	rw->getMetrics(width, height, depth, left, top);
+
+	const OIS::MouseState &ms = mMouse->getMouseState();
+	ms.width = width;
+	ms.height = height;
+}
+
+//Unattach OIS before window shutdown (very important under Linux)
+void Main::windowClosed(Ogre::RenderWindow* rw)
+{
+	//Only close for window that created OIS (the main window in these demos)
+	if (rw == mWindow)
+	{
+		if (mInputManager)
+		{
+			mInputManager->destroyInputObject(mMouse);
+			mInputManager->destroyInputObject(mKeyboard);
+
+			OIS::InputManager::destroyInputSystem(mInputManager);
+			mInputManager = 0;
+		}
+	}
+}
 
 //Main 
 #if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
--- Project_Feast/Project_Feast/Main.h
+++ Project_Feast/Project_Feast/Main.h
@@ -6,10 +6,16 @@
 #include "InputManager.h"
 #include "EnemyManager.h"
 
+#include <OISEvents.h>
+#include <OISInputManager.h>
+#include <OISKeyboard.h>
+#include <OISMouse.h>
+
 #include <SdkCameraMan.h>
+#include <SdkTrays.h>
 
 
-class Main: public Ogre::FrameListener, Ogre::WindowEventListener
+class Main : public Ogre::FrameListener, Ogre::WindowEventListener, public OIS::KeyListener, public OIS::MouseListener, OgreBites::SdkTrayListener
 {
 public:
 	Main();
@@ -22,14 +28,34 @@ protected:
 	Ogre::String mResourcesCfg;
 	Ogre::String mPluginsCfg;
 	Ogre::RenderWindow* mWindow;
+	Ogre::OverlaySystem *mOverlaySystem;
 	Player player;
 	MainCamera* mMainCamera;
 	EnemyManager enemyManager;
+	OgreBites::InputContext mInputContext;
+	OgreBites::SdkTrayManager* mTrayMgr;
 	OgreBites::SdkCameraMan* mCameraMan;      // basic camera controller
-	
+	OgreBites::ParamsPanel* mDetailsPanel;    // sample details panel
+
+	// OIS Input devices
+	OIS::InputManager* mInputManager;
+	OIS::Mouse*    mMouse;
+	OIS::Keyboard* mKeyboard;
 	
 	// Ogre::FrameListener
 	virtual bool frameRenderingQueued(const Ogre::FrameEvent& evt);
+
+	// OIS::KeyListener
+	virtual bool keyPressed(const OIS::KeyEvent &arg);
+	virtual bool keyReleased(const OIS::KeyEvent &arg);
+	// OIS::MouseListener
+	virtual bool mouseMoved(const OIS::MouseEvent &arg);
+	virtual bool mousePressed(const OIS::MouseEvent &arg, OIS::MouseButtonID id);
+	virtual bool mouseReleased(const OIS::MouseEvent &arg, OIS::MouseButtonID id);
+
+	// Ogre::WindowEventListener
+	virtual void windowResized(Ogre::RenderWindow* rw);
+	virtual void windowClosed(Ogre::RenderWindow* rw);
 
 private:
 	bool processUnbufferedInput(const Ogre::FrameEvent& fe);
