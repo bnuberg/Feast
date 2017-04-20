65f9cf9a90c98c5038f62e3a47466127828ec7fd
Glenn <glenn@haygarth.com>
2017-04-20 08:55:14 +0200

    body part visually different with color

--- Project_Feast/Project_Feast/BodyPart.cpp
+++ Project_Feast/Project_Feast/BodyPart.cpp
@@ -21,10 +21,20 @@ void BodyPart::Spawn(Ogre::Vector3 position)
 	Ogre::Vector3 target = Ogre::Vector3(0, 0, 0);
 
 	// Create a body part entity with the right mesh
-	Ogre::Entity* bodyPartEntity = mgr.mSceneMgr->createEntity("cube.mesh");
+	Ogre::Entity *bodyPartEntity = mgr.mSceneMgr->createEntity("cube.mesh");
 
 	// Add the node to the scene
 	Ogre::SceneNode* bodyPartNode = mgr.mSceneMgr->getRootSceneNode()->createChildSceneNode(position);
 	bodyPartNode->attachObject(bodyPartEntity);
 	bodyPartNode->setScale(0.2, 0.2, 0.2);
+
+	Ogre::MaterialPtr bodyPartMat = bodyPartEntity->getSubEntity(0)->getMaterial();
+	bodyPartMat->getTechnique(0)->getPass(0)->setAmbient(1, 0, 0);
+	bodyPartMat->getTechnique(0)->getPass(0)->setDiffuse(1, 0, 0, 0);
+	bodyPartEntity->setMaterialName(bodyPartMat->getName());
+
+	//m_pMat = m_pEntity->getSubEntity(0)->getMaterial();
+	//m_pMat->getTechnique(0)->getPass(0)->setAmbient(1, 0, 0);
+	//m_pMat->getTechnique(0)->getPass(0)->setDiffuse(1, 0, 0, 0);
+	//m_pEntity->setMaterialName(m_pMat->getName());
 }
--- Project_Feast/Project_Feast/BodyPart.h
+++ Project_Feast/Project_Feast/BodyPart.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "OgreEntity.h"
+#include "OgreSubEntity.h"
 
 class BodyPart
 {
