a431fb375f587541cb3e37fbe613ff3e5aa8f228
Brian Nuberg <bnuberg@live.nl>
2017-05-08 10:05:03 +0200

    Revert "Removed useless comment"
    This reverts commit 057a7143b09ee0a825b1bee99cc01fcdc234ef08.

--- Project_Feast/Project_Feast/BodyPart.cpp
+++ Project_Feast/Project_Feast/BodyPart.cpp
@@ -25,14 +25,17 @@ void BodyPart::Spawn(Ogre::Vector3 position)
 	Ogre::Vector3 target = Ogre::Vector3(0, 0, 0);
 
 	// Create a body part entity with the right mesh
-	Ogre::Entity *bodypartentity = mgr.mSceneMgr->createEntity(mesh);
+	Ogre::Entity *bodyPartEntity = mgr.mSceneMgr->createEntity(mesh);
 
 	// Add the node to the scene
 	bodyPartNode = mgr.mSceneMgr->getRootSceneNode()->createChildSceneNode(position);
-	bodyPartNode->attachObject(bodypartentity);
+	bodyPartNode->attachObject(bodyPartEntity);
 	bodyPartNode->setScale(0.2, 0.2, 0.2);
 
-	
+	//Ogre::MaterialPtr bodyPartMat = bodyPartEntity->getSubEntity(0)->getMaterial();
+	//bodyPartMat->getTechnique(0)->getPass(0)->setAmbient(0, 1, 0);
+	//bodyPartMat->getTechnique(0)->getPass(0)->setDiffuse(0, 1, 0, 0);
+	//bodyPartEntity->setMaterialName(bodyPartMat->getName());
 }
 
 
