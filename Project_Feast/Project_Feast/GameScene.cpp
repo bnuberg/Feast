32522701fb3f2bc3d1417713493247c3952d426e
Brian Nuberg <bnuberg@live.nl>
2017-05-17 15:05:17 +0200

    Refactored main a lot( no scene loading and ui anymore) and added level loader so you can load different scenes (yet to be tested fully)

---
+++ Project_Feast/Project_Feast/GameScene.cpp
@@ -0,0 +1,96 @@
+#include "GameScene.h"
+#include <OgreLight.h>
+#include "GameManager.h"
+#include <SdkCameraMan.h>
+
+#include <OgreViewport.h>
+#include <OgreEntity.h>
+#include <OgreMeshManager.h>
+#include "SoundManager.h"
+
+
+GameScene::GameScene()
+{
+}
+
+
+GameScene::~GameScene()
+{
+}
+
+void GameScene::CreateScene(Ogre::SceneManager* sceneManager, Ogre::RenderWindow* mWindow)
+{
+	GameManager& mgr = GameManager::getSingleton();
+	mainCamera->CameraInstance();
+
+	mgr.cameraMan = new OgreBites::SdkCameraMan(mgr.mCamera);   // create a default camera controller
+	mgr.cameraMan->setStyle(OgreBites::CameraStyle::CS_ORBIT);
+
+	Ogre::Viewport* vp = mWindow->addViewport(mgr.mCamera);
+	
+	vp->setBackgroundColour(Ogre::ColourValue(0, 0, 0));
+	mgr.mCamera->setAspectRatio(
+		Ogre::Real(vp->getActualWidth()) /
+		Ogre::Real(vp->getActualHeight()));
+
+	Ogre::TextureManager::getSingleton().setDefaultNumMipmaps(5);
+	Ogre::ResourceGroupManager::getSingleton().initialiseAllResourceGroups();
+
+	//---Create the scene---
+
+	Ogre::Plane plane(Ogre::Vector3::UNIT_Y, -2);
+
+	Ogre::MeshManager::getSingleton().createPlane(
+		"ground",
+		Ogre::ResourceGroupManager::DEFAULT_RESOURCE_GROUP_NAME,
+		plane,
+		1500, 1500, 20, 20,
+		true,
+		1, 5, 5,
+		Ogre::Vector3::UNIT_Z);
+
+	Ogre::Entity* groundEntity = sceneManager->createEntity("ground");
+	sceneManager->getRootSceneNode()->createChildSceneNode()->attachObject(groundEntity);
+	groundEntity->setMaterialName("Examples/Rockwall");
+	groundEntity->setCastShadows(false);
+
+	// Create a player entity with the right mesh
+	Ogre::Entity* playerEntity = GameManager::getSingleton().mSceneMgr->createEntity("Hammermesh", "RightArm_Hammer.mesh");
+
+	// Add the node to the scene
+	Ogre::Vector3 startingPosition = Ogre::Vector3(1000, -200, 50);
+	Ogre::SceneNode* playerNode = sceneManager->getRootSceneNode()->createChildSceneNode("hammerarm", startingPosition);
+	playerNode->attachObject(playerEntity);
+
+	// Instantiate the player
+	player.Init();
+
+	// Initialize the enemy manager
+	mgr.mEnemyManager.Init();
+
+	// Create an ambient light
+	sceneManager->setAmbientLight(Ogre::ColourValue(.5, .5, .5));
+	Ogre::Light* light = sceneManager->createLight("MainLight");
+	light->setPosition(20, 80, 50);
+
+	// Bind the cameraman to the player
+	mgr.cameraMan->setTarget(sceneManager->getSceneNode("PlayerHeadNode"));
+	//mCameraMan->setYawPitchDist(Ogre::Radian(0), Ogre::Radian(1.0472), Ogre::Real(500));
+
+	
+
+	mgr.ui.Init();
+}
+
+void GameScene::Update()
+{
+	GameManager& mgr = GameManager::getSingleton();
+
+	mgr.ui.ShowHud(player);
+
+	//Ogre::Real dist = (mgr.mCamera->getPosition() - mCameraMan->getTarget()->_getDerivedPosition()).length();
+	//mCameraMan->setYawPitchDist(mgr.mCamera->getOrientation().getYaw(), Ogre::Radian(1.0472), dist);
+	mgr.cameraMan->setYawPitchDist(Ogre::Radian(0), Ogre::Radian(0.349066), Ogre::Real(380));
+	
+}
+
