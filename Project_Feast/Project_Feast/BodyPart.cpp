8b1072ea4810c820b8cb283865db396bd71e828a
Brian Nuberg <bnuberg@live.nl>
2017-05-26 11:15:10 +0200

    Cleaned up bodypart

--- Project_Feast/Project_Feast/BodyPart.cpp
+++ Project_Feast/Project_Feast/BodyPart.cpp
@@ -5,12 +5,6 @@
 
 BodyPart::BodyPart()
 {
-	if (rand() % 2 == 0)
-		type = 0;
-	else
-		type = 1;
-
-	//attackType = new AbilityAttackAOE();
 }
 
 
@@ -41,26 +35,12 @@ void BodyPart::Spawn(Ogre::Vector3 position, Ogre::String bodypart)
 	bodyPartNode = mgr.mSceneMgr->getRootSceneNode()->createChildSceneNode(position);
 	bodyPartNode->attachObject(bodyPartEntity);
 	bodyPartNode->setScale(0.2, 0.2, 0.2);
-
-	//Ogre::MaterialPtr bodyPartMat = bodyPartEntity->getSubEntity(0)->getMaterial();
-	//bodyPartMat->getTechnique(0)->getPass(0)->setAmbient(0, 1, 0);
-	//bodyPartMat->getTechnique(0)->getPass(0)->setDiffuse(0, 1, 0, 0);
-	//bodyPartEntity->setMaterialName(bodyPartMat->getName());
 	
 }
 
 void BodyPart::Drop(Ogre::Vector3 position)
 {
 	GameManager& mgr = GameManager::GetSingleton();
-
-	/*if (mesh == "cube.mesh")
-	{
-		type = 0;
-	}
-	else if (mesh == "sphere.mesh")
-	{
-		type = 1;
-	}*/
 
 	Ogre::Entity *bodyPartEntity = mgr.mSceneMgr->createEntity(mesh);
 
