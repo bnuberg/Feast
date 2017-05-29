90a35cea6258adc442e3b9713fe8b3e03e992192
Glenn <glenn@haygarth.com>
2017-05-29 14:55:15 +0200

    Wave spawns when previous wave is dead
    Wave count is tracked
    Wave clear timer is tracked

--- Project_Feast/Project_Feast/Dungeon.h
+++ Project_Feast/Project_Feast/Dungeon.h
@@ -14,6 +14,6 @@ public:
 private:
 	void LoadScene(SceneManager* sceneManager);
 	const String meshName = "pPlane1.mesh";
-	Vector3 playerSpawnPoint = Vector3(0, 0, 200);
+	Vector3 playerSpawnPoint = Vector3(0, 0, -500);
 	//Mesh map;
 };
